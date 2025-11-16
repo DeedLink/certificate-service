e5a92215947e2c151d31916621d85a6ee84688f9
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-11-16 12:30:05 +0530

    update

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use('/api/certificates', certificateRoutes);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5009;
 
 connectDB()
   .then(() => {
